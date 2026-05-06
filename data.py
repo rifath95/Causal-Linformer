526128e2e11478f27965be42e782b37f18bb92f5
Rifath Khan <rifathkhantheo@gmail.com>
2026-05-05 21:28:47 -0700

    Git Init

---
+++ data.py
@@ -0,0 +1,50 @@
+import torch
+
+from config import *
+
+# Opening the dataset and text is a long string
+with open('tinyShakespeare.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# Extracting the characters in text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# Encoding & decoding
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+def encode(s): return [stoi[ch] for ch in s]
+def decode(l): return ''.join(itos[i] for i in l)
+
+
+# train vs val data split
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+# Data loading
+
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (micro_batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+@torch.no_grad()
+def estimate_loss(model):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, cross_entropy_loss, load_balance_loss = model(X, Y)
+            losses[k] = cross_entropy_loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
